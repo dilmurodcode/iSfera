f4cc29199b1ef4ee268c2265a5abfb8c4b576b53
Dilmurod <@eshmamatovdilmurod7@gmail.com>
2025-03-25 18:57:59 +0500

    c

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from app.models import Connection, ClientCompany, ServiceContact
+
 
 # Register your models here.
+@admin.register(Connection)
+class AdminConnection(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'phone', 'email')
+    list_display_links = ('id', 'full_name', 'phone', 'email')
+    list_filter = ('full_name',)
+
+
+@admin.register(ClientCompany)
+class AdminClientCompany(admin.ModelAdmin):
+    list_display = ('full_name', 'image', 'position', 'type')
+    list_display_links = ('full_name', 'image', 'position', 'type')
+
+
+@admin.register(ServiceContact)
+class AdminServiceContact(admin.ModelAdmin):
+    list_display = ('connection', 'service', 'category')
+    list_display_links = ('connection', 'service', 'category')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Connection(models.Model):
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+
+class ClientCompany(models.Model):
+
+    class TypeChoice(models.TextChoices):
+        CUSTOMER = 'customer', 'CUSTOMER'
+        COMPANY = 'company', 'COMPANY'
+
+    full_name = models.CharField(max_length=255)
+    image = models.ImageField()
+    position = models.CharField(max_length=255, null=True, blank=True)
+    order = models.IntegerField(default=0)
+    type = models.CharField(max_length=255, choices=TypeChoice.choices, default=TypeChoice.CUSTOMER)
+
+
+    '''Service connection'''
+class ServiceContact(models.Model):
+    connection = models.CharField(max_length=255)
+    service = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
