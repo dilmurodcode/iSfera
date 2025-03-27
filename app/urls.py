965413ed9fec1417a5a24908465e0abbfcbdbf4f
Nozima <abduazizovanozima7@gmail.com>
2025-03-27 18:32:01 +0500

    qw

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("connections-list/", views.ConnectionListAPIViews.as_view())
+    path("connections/", views.ConnectionListAPIViews.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from .models import *
-from rest_framework import views
+from rest_framework import views, generics
 from . import serializers
 from rest_framework.response import Response
 
-class ConnectionListAPIViews(views.APIView):
-
-    def get(self, request):
-        data = Connection.objects.filter()
-        serializer = serializers.ConnectionSerializer(data, many=True)
-        return Response(
-            data=serializer.data,)
+class ConnectionListAPIViews(generics.ListAPIView):
+    queryset = Connection.objects.all()
+    serializer_class = serializers.ConnectionSerializer
